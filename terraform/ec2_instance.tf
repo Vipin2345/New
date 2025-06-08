a6c92dcccaeb1d4a91a4abbd8db2140a9dbef421
reap <reap7979@gmail.com>
2025-06-08 22:38:50 +0530

    modified

--- terraform/ec2_instance.tf
+++ terraform/ec2_instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "name" {
     ami = var.instance_ami
-    instance_type = t2.micro
+    instance_type = "t2.micro"
     count = 2
     tags = {
       name = "terraform-${count.index}"
